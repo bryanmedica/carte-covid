8efc1a19393e2fc52c9af2680cdc03099e1f8efd
lmaignan <leo.maignan@epitech.eu>
2020-06-09 21:28:25 +0200

    💄 Updating publish stylesheet

---
+++ public/javascripts/publish.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#uploadFile').on("change", function() {
+        $("#labelFile").text("Le fichier est prêt !");
+        $("#uploadButton").css({"background-color": "rgb(72, 180, 0)", "border-color" : "rgb(72, 180, 0)"})
+        uploadFile();
+    });
+
+    $("#submitButton").mouseover(function() {
+        if ($("#restaurantName").val() == "" || !($("#uploadFile").val()))
+            $(this).css({"background-color": "#fc5151", "border-color" : "#fc5151"});
+        else
+            $(this).css({"background-color": "#48b400", "border-color" : "#48b400"});
+    }).mouseout(function() {
+        $(this).css({"background-color": "#5f7e96", "border-color" : "#5f7e96"});
+    });
+
+    $("#submitButton").click(function() {
+        if ($("#restaurantName").val() == "" || !($("#uploadFile").val()))
+            event.preventDefault();
+    });
+});
